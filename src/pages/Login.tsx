53dea68e6d4a22dc60ade4c06c9a7e2ccc8ca82e
Muruganandham3 <muruganandham.elangovan@gmail.com>
2026-06-30 08:29:07 +0530

    LOGIN page gave padding for the email code changes

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -12,17 +12,31 @@ const Login = () => {
   const {register, handleSubmit, formState: { errors } } = useForm<LoginForm>();
   return (
     <div className="h-screen flex items-center justify-center bg-gray-100">
-    <Card className={'w-full max-w-md h-2/4'}>
-    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4 pt-20">
-      <input className="border border-b-gray-100 px-3 py-2 rounded-md mt-5" placeholder="Enter a email" {...register("email",{required: 'Email is required'})}/>
-      {errors.email && <span>{errors.email.message}</span>}
-      <input className="border border-b-gray-100 px-3 py-2 rounded-md" placeholder="Enter a password" {...register("password",{required: 'Password is required'})}/>
-      {errors.password && <span>{errors.password.message}</span>} 
-      <input placeholder="Enter a gender" {...register("gender",{required:'Gender is required'})}/>
-      {errors.gender &&  <span>errors.password?.message</span>}
-      <button className="bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700" type="submit">Login</button>
-    </form>
-    </Card>
+   <Card className="w-full max-w-md h-2/4 !pt-16">
+  <form onSubmit={handleSubmit(onSubmit)} className="flex w-full flex-col gap-6">
+    <input
+      className="border border-gray-300 px-3 py-2 rounded-md"
+      placeholder="Enter a email"
+      {...register("email", { required: "Email is required" })}
+    />
+
+    <input
+      className="border border-gray-300 px-3 py-2 rounded-md"
+      placeholder="Enter a password"
+      {...register("password", { required: "Password is required" })}
+    />
+
+    <input
+      className="border border-gray-300 px-3 py-2 rounded-md"
+      placeholder="Enter a gender"
+      {...register("gender", { required: "Gender is required" })}
+    />
+
+    <button className="bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700" type="submit">
+      Login
+    </button>
+  </form>
+</Card>
     </div>
   )
 }
