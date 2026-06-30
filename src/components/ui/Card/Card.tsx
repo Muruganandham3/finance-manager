6d17aab4be10c53eec488a911abf9a4e8a0926dc
Muruganandham3 <muruganandham.elangovan@gmail.com>
2026-06-30 07:59:54 +0530

    login page design

--- src/components/ui/Card/Card.tsx
+++ src/components/ui/Card/Card.tsx
@@ -2,7 +2,7 @@ import type { CardProps } from './CardTypes'
 
 export default function Card({children,className=""}:CardProps) {
   return (
-    <div className={`card ${className}`}>
+    <div className={`bg-white rounded-xl shadow-lg p-6 border border-gray-200 ${className}`}>
         {children}
     </div>
     
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -11,17 +11,19 @@ const onSubmit=(data:LoginForm)=>{
 const Login = () => {
   const {register, handleSubmit, formState: { errors } } = useForm<LoginForm>();
   return (
-    <Card className={'mt-4'}>
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input placeholder="Enter a email" {...register("email",{required: 'Email is required'})}/>
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+    <Card className={'w-full max-w-md h-2/4'}>
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4 pt-20">
+      <input className="border border-b-gray-100 px-3 py-2 rounded-md mt-5" placeholder="Enter a email" {...register("email",{required: 'Email is required'})}/>
       {errors.email && <span>{errors.email.message}</span>}
-      <input placeholder="Enter a password" {...register("password",{required: 'Password is required'})}/>
+      <input className="border border-b-gray-100 px-3 py-2 rounded-md" placeholder="Enter a password" {...register("password",{required: 'Password is required'})}/>
       {errors.password && <span>{errors.password.message}</span>} 
       <input placeholder="Enter a gender" {...register("gender",{required:'Gender is required'})}/>
       {errors.gender &&  <span>errors.password?.message</span>}
-      <button type="submit">Login</button>
+      <button className="bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700" type="submit">Login</button>
     </form>
     </Card>
+    </div>
   )
 }
 
