05fabbd8dd0209b9eb8d9e2f82bd71f041901702
Muruganandham3 <muruganandham.elangovan@gmail.com>
2026-06-30 15:00:27 +0530

    login page developed with hardcoded credntials

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,42 +1,56 @@
-import  Card  from "@components/ui/Card/Card";
+import Card from "@components/ui/Card/Card";
 import { useForm } from "react-hook-form";
-type LoginForm={
-  email:String,
-  password:String,
-  gender:String
-}
-const onSubmit=(data:LoginForm)=>{
-  console.log(data);
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+
+type LoginForm = {
+  email: String,
+  password: String,
+  gender: String
 }
 const Login = () => {
-  const {register, handleSubmit, formState: { errors } } = useForm<LoginForm>();
+  const [loginError,setLoginError]=useState(false);
+  const nagivate=useNavigate();
+
+  const onSubmit = (data: LoginForm) => {
+    if(data.email=="admin" && data.password=="123456"){
+        nagivate("/dashboard");
+    }else{
+      setLoginError("Invalid Credentials");
+    }
+    console.log(data);
+  }
+  const { register, handleSubmit, formState: { errors } } = useForm<LoginForm>();
   return (
     <div className="h-screen flex items-center justify-center bg-gray-100">
-   <Card className="w-full max-w-md h-2/4 !pt-16">
-  <form onSubmit={handleSubmit(onSubmit)} className="flex w-full flex-col gap-6">
-    <input
-      className="border border-gray-300 px-3 py-2 rounded-md"
-      placeholder="Enter a email"
-      {...register("email", { required: "Email is required" })}
-    />
+      <Card className="w-full max-w-md min-h-[320px] pt-16">
+        <form onSubmit={handleSubmit(onSubmit)} className="flex w-full min-h-[320px] h-full  flex-col gap-6 justify-center items-center">
+          <div className="w-sm">
+          <input
+            className="border border-gray-300 px-3 py-2 rounded-md w-sm !p-0.5  focus:border-blue-600 focus:border-2 focus:outline-none"
+            placeholder="Enter a email"
+            {...register("email", { required: "Email is required" })}
+          />
+          {errors.email && (<p className="text-red-500 text-sm">{errors.email.message}</p>)}
+          </div>
 
-    <input
-      className="border border-gray-300 px-3 py-2 rounded-md"
-      placeholder="Enter a password"
-      {...register("password", { required: "Password is required" })}
-    />
+          <div className="w-sm">
+          <input
+            className="border border-gray-300 px-3 py-2 rounded-md w-sm !p-0.5 focus:border-blue-600 focus:border-2 focus:outline-none"
+            placeholder="Enter a password"
+            {...register("password", { required: "Password is required" })}
+          />
+          {errors.password && (<p className="text-red-500 text-sm">{errors.password.message}</p>)}
+          </div>
 
-    <input
-      className="border border-gray-300 px-3 py-2 rounded-md"
-      placeholder="Enter a gender"
-      {...register("gender", { required: "Gender is required" })}
-    />
+          {loginError && (<p className="text-red-500 text-sm">{loginError}</p>)}
 
-    <button className="bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700" type="submit">
-      Login
-    </button>
-  </form>
-</Card>
+          <button className="bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 w-30 !p-0.5" type="submit">
+            Login
+          </button>
+        </form>
+      </Card>
     </div>
   )
 }
